687d0ce4e08834edd2a078d18dad1d78ccfbd004
blazen777 <johnrenny@my.smccd.edu>
2019-08-21 06:53:28 +0300

    First bot telegram version

--- bot.py
+++ bot.py
@@ -1,1 +1,39 @@
-print("Hello github")
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+from settings import token
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename='bot.log'
+)
+
+def greeting(bot, update):
+    msg = "Bots is comming!!!"
+    logging.info(msg)
+    update.message.reply_text(msg)
+
+def talk_to_me(bot, update):
+    user_msg = f"Привет {update.message.chat.first_name}!! \nТы написал: {update.message.text}"
+    logging.info(f'User: {update.message.chat.username}, Chat id: {update.message.chat.id}, Message: {update.message.text}')
+    update.message.reply_text(user_msg)
+
+
+def main():
+    bot = Updater(token)
+
+    logging.info('Bot is running')
+    # Добавить обработчик
+    dp = bot.dispatcher
+    # Обработчик комманд
+    dp.add_handler(CommandHandler("start", greeting))
+    # Обработчик сообщений
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    # начать проверку сообщений
+    bot.start_polling()
+    # работать до момента остановки
+    bot.idle()
+
+
+main()
